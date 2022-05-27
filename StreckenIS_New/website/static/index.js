6cd16c33e0dbd21af4582220009b171792c34713
tweissenbacher <t.weissenbacher@yahoo.at>
2022-05-27 15:11:06 +0200

    Anpassung index.js

--- StreckenIS_New/website/static/index.js
+++ StreckenIS_New/website/static/index.js
@@ -11,6 +11,38 @@ function deleteTrainstation(trainstationId) {
     method: "POST",
     body: JSON.stringify({ trainstationId: trainstationId }),
   }).then((_res) => {
-    window.location.href = "/";
+    window.location.href = "/all_trainstations";
+  });
+}
+function deleteSection(sectionId) {
+  fetch("/delete-section", {
+    method: "POST",
+    body: JSON.stringify({ sectionId: sectionId }),
+  }).then((_res) => {
+    window.location.href = "/all_sections";
+  });
+}
+function deleteRoute(routeId) {
+  fetch("/delete-route", {
+    method: "POST",
+    body: JSON.stringify({ routeId: routeId }),
+  }).then((_res) => {
+    window.location.href = "/all_routes";
+  });
+}
+function deleteWarning(warningId) {
+  fetch("/delete-warning", {
+    method: "POST",
+    body: JSON.stringify({ warningId: warningId }),
+  }).then((_res) => {
+    window.location.href = "/all_warnings";
+  });
+}
+function deleteUser(userId) {
+  fetch("/delete-user", {
+    method: "POST",
+    body: JSON.stringify({ userId: userId }),
+  }).then((_res) => {
+    window.location.href = "/users";
   });
 }
