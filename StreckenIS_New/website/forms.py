7e8754b94401009c634c77569e58344e4e3b3d10
tweissenbacher <t.weissenbacher@yahoo.at>
2022-06-07 00:55:37 +0200

    Update: views, html, relationships

---
+++ StreckenIS_New/website/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, DateField, BooleanField
+from wtforms.validators import DataRequired, Length
+
+
+class EditProfileForm(FlaskForm):
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[Length(min=3, max=150)])
+    email = StringField('email', validators=[Length(min=3, max=150)])
+    password = StringField('password', validators=[Length(min=3, max=150)])
+    birthday = DateField('birthday')
+    admin = BooleanField('admin')
+    submit = SubmitField('submit')
+
+
+class EditTrainstationForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    address = StringField('address', validators=[Length(min=3, max=150)])
+    submit = SubmitField('submit')
+
+
+class EditSectionForm(FlaskForm):
+    start = StringField('start', validators=[DataRequired()])
+    end = StringField('end', validators=[Length(min=3, max=150)])
+    track = StringField('track', validators=[Length(min=3, max=150)])
+    fee = StringField('fee')
+    time = StringField('time')
+    submit = SubmitField('submit')
+
+
+class EditRouteForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    start = StringField('start', validators=[Length(min=3, max=150)])
+    end = StringField('end', validators=[Length(min=3, max=150)])
+    route_sections = StringField('route_sections', validators=[Length(min=3, max=150)])
+    v_max = StringField('v_max')
+    submit = SubmitField('submit')
+
+
+class EditWarningsForm(FlaskForm):
+    warnings = StringField('warnings', validators=[DataRequired()])
+    submit = SubmitField('submit')
